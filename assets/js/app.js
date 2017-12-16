2dac8d32da71ff7360652f57de84284a68b0f07d
CeliaCha <chazel.celia@gmail.com>
2017-12-16 14:52:03 +0100

    réécriture du callback de meteoRequest()

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,35 +4,33 @@
 
 	$("input").click(function() {
 		$("input").val("");
+		$("input").css("color", "black");
+
 	})
 
 	$("#cityChoice").click(function() {
 		var myRequest = $("input").val();
-		// push la réponse Ajax dans l'array cityArray
-		meteoRequest(myRequest, function(res){ 
-			cityArray.push(res);
-			switchElement(cityArray);
-		});
+		meteoRequest(myRequest,updateCards);
 	})
 
+
+	// requête Ajax
 	function meteoRequest(cityRequest, callback) {
 		$.ajax({
 			url : 'https://api.openweathermap.org/data/2.5/weather?q=' + cityRequest + '&units=metric&APPID=ee43646aad159fa17c9141272573d285', 
 			type : 'GET',
 			dataType: 'json',
 			success: function(res) {
-				// on stocke les données dans un array...
 				var myData = {
-					"Ville": res.name,
-					"Longitude": res.coord.lon,
-					"Latitude": res.coord.lat,
+					"Votre recherche" : res.name,
+					"Longitude" : res.coord.lon,
+					"Latitude" : res.coord.lat,
 					"Vitesse du vent": res.wind.speed,
-					"Humidité":res.main.humidity,
-					"Pression":res.main.pressure,
-					"Température min":res.main.temp_min,
-					"Température max":res.main.temp_max,
+					"Humidité": res.main.humidity,
+					"Pression": res.main.pressure,
+					"Température min": res.main.temp_min + ' C°',
+					"Température max":res.main.temp_max + ' C°',
 				};
-				// qu'on retourne via le callback
 				callback(myData);
 			},
 			error: function(err) {
@@ -46,19 +44,19 @@
 
 
 
-	function switchElement(cityArray) {
-		console.log(cityArray.length);
-		var newCity = cityArray[cityArray.length-1];
-		var lastCity = cityArray[cityArray.length-2];
-		console.log(newCity);
-		console.log(lastCity);
-		$('#fiche2').html("");
-		for (var i in newCity) {
-			$('#fiche2').append("<div class=\"description\">" + i + " : " + newCity[i] + "</div>");
+		// callback pour meteoRequest() :
+		function updateCards(myData) { 
+			cityArray.push(myData);
+			var newCity = cityArray[cityArray.length-1];
+			var lastCity = cityArray[cityArray.length-2];
+			$('#fiche2').html("");
+			for (var i in newCity) {
+				$('#fiche2').append("<div class=\"description\">" + i + " : " + newCity[i] + "</div>");
+			}
+			$('#fiche1').html("");
+			for (var i in lastCity) {
+				$('#fiche1').append("<div class=\"description\">" + i + " : " + lastCity[i] + "</div>");
+			};
 		}
-		$('#fiche1').html("");
-		for (var i in lastCity) {
-			$('#fiche1').append("<div class=\"description\">" + i + " : " + lastCity[i] + "</div>");
-		}
-	}
+
 
